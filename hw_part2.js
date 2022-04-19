82c271efa4ad659f52d6b9a5c505317a406644a3
Anastasia-cod <70442245+Anastasia-cod@users.noreply.github.com>
2022-04-19 20:59:11 +0400

    Update hw_part2.js

--- hw_part2.js
+++ hw_part2.js
@@ -87,23 +87,20 @@ checkAge('21')
 //4***:Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
 
 let age = prompt('Enter your age:')
-let enter = Number(age);
+checkAge(age)
 let age_2 = 18;
 let age_3 = 60;
 
-checkAge(age)
-
 function checkAge(age){
-        
-    if(typeof(age) !== 'number' && typeof(age) !== 'string' || typeof(age) == 'string' && typeof(enter) !== 'number'){
+    if(isNaN(Number(age))){
         alert("ERROR")
-    } else if(age < age_2){
-        alert('You don’t have access cause your age is' + ' ' + age + '. ' + 'It’s less then' + ' ' + age_2);
-    } else if(age >= age_2 & age < age_3){
-        alert('Welcome');
-    } else if(age >= age_3){
-        alert('Keep calm and look Culture channel');
-    } else{
-        alert('Technical work');
-    }
+    } else {
+        if (age < age_2){
+            alert('You don’t have access cause your age is' + ' ' + age + '. ' + 'It’s less then' + ' ' + age_2);
+        } else if(age >= age_2 && age < age_3){
+            alert('Welcome');
+        } else if(age >= age_3){
+            alert('Keep calm and look Culture channel');
+        } 
+    }  
 }
