d74d09e3816a28225e1dcc7174d5157b8bf04d95
Anastasia-cod <70442245+Anastasia-cod@users.noreply.github.com>
2022-04-19 20:50:44 +0400

    Update hw_part2.js

--- hw_part2.js
+++ hw_part2.js
@@ -61,23 +61,19 @@ checkAge('text');
 let age_2 = 18;
 let age_3 = 60;
 
-
 const checkAge = function(age){
-    let test = Number(age);
-
-    if(typeof(age) !== 'number' && typeof(age) !== 'string' || typeof(age) == 'string' && typeof(test) !== 'number'){
+    if(isNaN(Number(age))){
         console.log("ERROR")
-    } else if(age < age_2){
-        console.log('You don’t have access cause your age is' + ' ' + age + '. ' + 'It’s less then' + ' ' + age_2);
-    } else if(age >= age_2 & age < age_3){
-        console.log('Welcome');
-    } else if(age >= age_3){
-        console.log('Keep calm and look Culture channel');
-    } else{
-        console.log('Technical work');
-    }
+    } else {
+        if (age < age_2){
+            console.log('You don’t have access cause your age is' + ' ' + age + '. ' + 'It’s less then' + ' ' + age_2);
+        } else if(age >= age_2 && age < age_3){
+            console.log('Welcome');
+        } else if(age >= age_3){
+            console.log('Keep calm and look Culture channel');
+        } 
+    }  
 }
-
 
 checkAge(true);
 checkAge('2');
